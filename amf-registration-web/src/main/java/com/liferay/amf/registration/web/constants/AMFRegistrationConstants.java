958d24b4090a96eef71a4e6ab89c9c871b904a37
timscottbell <77371218@liferay.com>
2018-05-16 16:55:08 -0700

    Initial commit

---
+++ amf-registration-web/src/main/java/com/liferay/amf/registration/web/constants/AMFRegistrationConstants.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.amf.registration.web.constants;
+
+/**
+ * @author Timothy Bell
+ */
+public class AMFRegistrationConstants {
+
+	public static final String SECURITY_QUESTION_1 =
+		"what-is-your-mother's-maiden-name";
+
+	public static final String SECURITY_QUESTION_2 =
+		"what-is-the-make-of-your-first-car";
+
+	public static final String SECURITY_QUESTION_3 =
+		"what-is-your-high-school-mascot";
+
+	public static final String SECURITY_QUESTION_4 =
+		"who-is-your-favorite-actor";
+
+	public static final String[] SECURITY_QUESTIONS = {
+		SECURITY_QUESTION_1, SECURITY_QUESTION_2, SECURITY_QUESTION_3,
+		SECURITY_QUESTION_4
+	};
+
+}
