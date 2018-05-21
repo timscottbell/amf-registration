b348dffc1271e534fa78ddee27f5071d9ea135cc
timscottbell <77371218@liferay.com>
2018-05-21 16:59:55 -0700

    Remove unneeded code

--- amf-service/src/main/resources/META-INF/sql/indexes.sql
+++ amf-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,3 +1,1 @@
 create index IX_FCB28A2F on AMFEvent (type_);
-
-create index IX_9EFFECA on AMF_AMFEvent (type_);
--- amf-service/src/main/resources/META-INF/sql/tables.sql
+++ amf-service/src/main/resources/META-INF/sql/tables.sql
@@ -6,12 +6,3 @@ create table AMFEvent (
 	type_ INTEGER,
 	ipAddress VARCHAR(75) null
 );
-
-create table AMF_AMFEvent (
-	amfEventId LONG not null primary key,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createTime LONG,
-	type_ INTEGER,
-	ipAddress VARCHAR(75) null
-);
