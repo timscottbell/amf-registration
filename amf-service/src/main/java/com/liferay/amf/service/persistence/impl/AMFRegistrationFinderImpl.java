c0e143890bd089914d706910ac7ffc5c1fc0e7f3
timscottbell <77371218@liferay.com>
2018-05-24 12:29:14 -0700

    Modify finder

--- amf-service/src/main/java/com/liferay/amf/service/persistence/impl/AMFRegistrationFinderImpl.java
+++ amf-service/src/main/java/com/liferay/amf/service/persistence/impl/AMFRegistrationFinderImpl.java
@@ -1,7 +1,6 @@
 package com.liferay.amf.service.persistence.impl;
 
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.service.persistence.impl.BasePersistenceImpl;
-
-public class AMFRegistrationFinderImpl extends BasePersistenceImpl<User> {
+import com.liferay.amf.service.persistence.AMFRegistrationFinder;
+public class AMFRegistrationFinderImpl
+	extends AMFRegistrationFinderBaseImpl implements AMFRegistrationFinder {
 }
